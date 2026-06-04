8144e82be3da8b8be491151495c0b6be467fd189
Miguel-Alves <miguelalvesdemoura2@gmail.com>
2026-06-04 10:46:11 -0400

    Adiciona suporte CORS

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ load_dotenv()
 
 # 3. CONFIGURAÇÕES INICIAIS
 app = FastAPI(title="API de Download TikTok")
+# HABILITAR CORS
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://tiktok-downloader-8y2.pages.dev"],   # ou ["https://tiktok-downloader-8y2.pages.dev"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 API_KEY = os.getenv("API_KEY")
 if not API_KEY:
